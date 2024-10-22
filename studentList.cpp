c9d20a0ebee3b213493aa750057f40c4f7ff25a2
U-BSD\419097 <419097@SHS-9YBF6X3.beaverton.k12.or.us>
2024-10-22 15:27:41 -0700

    everything else. need fixes and comments

--- studentList.cpp
+++ studentList.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <cstring>
+#include <iomanip>
 
 using namespace std;
 
@@ -13,11 +15,11 @@ struct Student {
 void adder(vector<Student*> &stu);
 void printer(vector<Student*> &stu);
 void deleter(vector<Student*> &stu);
-void quitter(vector<Student*> &stu);
+void quitter(vector<Student*> &stu, bool &input);
 
 int main()
 {
-  Student george;
+  /*Student george;
   vector<Student*> stu;
   cin >> george.first;
   george.id = 123;
@@ -25,8 +27,33 @@ int main()
   Student* ptr = &george;
   stu.push_back(ptr);
   cout << stu[0]->first << stu[0]->id << stu[0]->gpa << endl;
-  adder(stu);
-  
+  adder(stu);*/
+
+  vector<Student*> stu;
+  bool input = true;
+  while (input == true) {
+    
+    cout << "Input a command." << endl;
+    char command[6] = "";
+
+    cin >> command; 
+    cin.ignore();
+    if (strcmp(command, "ADD") == 0) {
+      adder(stu);
+    }
+    else if (strcmp(command, "PRINT") == 0) {
+      printer(stu);
+    }
+    else if (strcmp(command, "DELETE") == 0) {
+      deleter(stu);
+    }
+    else if (strcmp(command, "QUIT") == 0) {
+      quitter(stu, input);
+    }
+    else {
+      cout << "Invalid input! Try again." << endl;
+    }
+  }
 }
 
 void adder(vector<Student*> &stu) {
@@ -43,4 +70,31 @@ void adder(vector<Student*> &stu) {
 
   stu.push_back(newStu);
   cout << newStu->first << " has been added." << endl;
+}
+
+void printer(vector<Student*> &stu) {
+  for (auto it = stu.begin(); it != stu.end(); ++it) {
+    cout << (*it)->first << " " << (*it)->last << ", " << (*it)->id << ", " <<
+      fixed << setprecision(2) << (*it)->gpa << endl;
+  }
+}
+
+void deleter(vector<Student*> &stu) {
+    int deleteID;
+    cout << "Enter the ID of the student to be deleted: ";
+    cin >> deleteID;
+
+    for (auto it = stu.begin(); it != stu.end();) {
+        if (deleteID == (*it)->id) {
+            cout << "Deleting student..." << endl;
+            stu.erase(it);
+        } else {
+            ++it; // Move to the next student only if not deleted
+        }
+    }
+}
+
+void quitter(vector<Student*> &stu, bool &input) {
+  cout << "Goodbye!" << endl;
+  input = false;
 }
